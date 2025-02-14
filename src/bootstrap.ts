5117c43b354eafa062e4e30b9820931844a70022
Yuan Ai <yuanai@letsrotate.com>
2025-02-14 15:14:50 +0800

    Convert app to shell app

---
+++ src/bootstrap.ts
@@ -0,0 +1,17 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Amplify } from 'aws-amplify';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'eu-central-1_H7mxQtWY8',
+      userPoolClientId: '5d8puntt738u8liac3hjr4b4nn',
+    },
+  },
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { Amplify } from 'aws-amplify';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { initFederation } from '@angular-architects/native-federation';
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'eu-central-1_H7mxQtWY8',
-      userPoolClientId: '5d8puntt738u8liac3hjr4b4nn',
-      //   signUpVerificationMethod: 'code',
-      //   loginWith: {
-      //     email: true,
-      //   },
-      //   userAttributes: {
-      //     email: { required: true },
-      //   },
-      //   passwordFormat: {
-      //     minLength: 8,
-      //     requireLowercase: true,
-      //     requireUppercase: true,
-      //     requireNumbers: true,
-      //     requireSpecialCharacters: true,
-      //   },
-    },
-  },
-});
-
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+initFederation('federation.manifest.json')
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
